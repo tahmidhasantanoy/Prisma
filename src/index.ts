e1effb89680fecbfcd836df0ea242e849d63db62
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2025-08-22 22:24:38 +0600

    Feat: Get all data from table using prisma

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,19 @@ const prisma = new client.PrismaClient();
 const operationalFunction = async () => {
 
 // Add table data using prisma
-  const result = await prisma.post.create({
-    data: {
-      title: "This is title",
-      content: "This is content",
-      author: "Tahmid Hasan",
-      hashTag: "#devs",
-    },
-  });
+//   const result = await prisma.post.create({
+//     data: {
+//       title: "This is title",
+//       content: "This is content",
+//       author: "Tahmid Hasan",
+//       hashTag: "#devs",
+//     },
+//   });
 
-  console.log(result);
+
+const getAllFromDB = await prisma.post.findMany()
+
+  console.log(getAllFromDB);
 };
 
 operationalFunction();
