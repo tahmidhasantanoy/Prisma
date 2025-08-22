90bf3a4af44405d9fc1f50d663c5f30e0bdab077
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2025-08-23 00:50:19 +0600

    Feat: Update multiple data using prisma

--- src/update.ts
+++ src/update.ts
@@ -5,17 +5,27 @@ import client = require("./generated/client");
 const prisma = new client.PrismaClient();
 
 const updateFunction = async () => {
-  const updateData = await prisma.post.update({
-    where: { id: 9 },
-    data: {
-      title: "Updated Title",
-      content: "Updated Content",
-      author: "Updated Author",
-      hashTag: "#updated",
-    },
-  });
+//   const updateData = await prisma.post.update({
+//     where: { id: 9 },
+//     data: {
+//       title: "Updated Title",
+//       content: "Updated Content",
+//       author: "Updated Author",
+//       hashTag: "#updated",
+//     },
+//   });
 
-  console.log(updateData);
+const updateMultipleData = await prisma.post.updateMany({
+    where: { id: { in: [1, 2, 3] } },
+    data: {
+        title: "Updated Title",
+        content: "Updated Content",
+        author: "Updated Author",
+        hashTag: "#updated",
+    },
+});
+
+  console.log(updateMultipleData);
 };
 
 updateFunction();
