2fd93e6784790d1fe4494d293849cedd89c9bc77
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2025-08-23 01:11:27 +0600

    Feat: Delete multiple data using prisma

--- src/delete.ts
+++ src/delete.ts
@@ -3,9 +3,17 @@ import client = require("./generated/client");
 const prisma = new client.PrismaClient();
 
 const deleteFunction = async () => {
-  const deleteSpecificData = await prisma.post.delete({
-    where: { id: 9 },
+  //   const deleteSpecificData = await prisma.post.delete({
+  //     where: { id: 9 },
+  //   });
+
+  const deleteMultipleData = await prisma.post.deleteMany({
+    where: {
+      published: false,
+    },
   });
+
+  console.log(deleteMultipleData);
 };
 
 deleteFunction();
